9f2aefaa997e749ef744a3717e15dbf69e4b5653
RubberSpring <143825964+RubberSpring@users.noreply.github.com>
2025-02-01 18:19:48 -0300

    Improved tests

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -2,7 +2,9 @@ from typer.testing import CliRunner
 from mathlamp.main import app
 
 from random import randrange
-from os import remove
+from shutil import rmtree
+from math import sqrt
+import os
 
 import pytest
 from jinja2 import Environment, FileSystemLoader
@@ -11,9 +13,19 @@ runner = CliRunner()
 
 environment = Environment(loader=FileSystemLoader("templates/"))
 
-@pytest.fixture
+
+@pytest.fixture(scope="session", autouse=True)
 def setup():
+    directory = "test-temp"
     print("\nSetting up resources...")
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    yield directory
+    print(f"\nRemoving {directory}...")
+    rmtree(directory)
+
+
+def test_arthimetic(setup):
     lines = [
         {"x": randrange(1, 15), "sign": "+", "y": randrange(1, 15)},
         {"x": randrange(1, 15), "sign": "-", "y": randrange(1, 15)},
@@ -21,23 +33,16 @@ def setup():
         {"x": randrange(1, 15), "sign": "/", "y": randrange(1, 15)},
         {"x": randrange(1, 15), "sign": "%", "y": randrange(1, 15)}
     ]
-    yield lines
-    print("\nRemoving tmp file...")
-    remove("tmp.lmp")
-
-
-def test_arthimetic(setup):
     template = environment.get_template("operator.txt")
-    with open("tmp.lmp", "w") as f:
+    with open(f"{setup}/arthimetic.lmp", "w") as f:
         print("\nWriting tmp file...")
-        f.write(template.render(lines=setup))
-    result = runner.invoke(app, "tmp.lmp")
+        f.write(template.render(lines=lines))
+    result = runner.invoke(app, f"{setup}/arthimetic.lmp")
     content = result.stdout.splitlines()
     assert result.exit_code == 0
-
     outputs = []
     print("Calculating...")
-    for math in setup:
+    for math in lines:
         match math["sign"]:
             case "+":
                 outputs.append(math["x"] + math["y"])
@@ -54,5 +59,23 @@ def test_arthimetic(setup):
             case "%":
                 outputs.append(math["x"] % math["y"])
     print("Checking...")
-    for c, o in zip(content,outputs):
+    for c, o in zip(content, outputs):
         assert float(c) == o
+
+
+def test_functions(setup):
+    template = environment.get_template("functions.txt")
+    context = {
+        "root": randrange(1, 13),
+        "x": randrange(1, 13),
+        "y": randrange(1, 5)
+    }
+    with open(f"{setup}/functions.lmp", "w") as f:
+        print("\nWriting tmp file...")
+        f.write(template.render(context))
+    result = runner.invoke(app, f"{setup}/functions.lmp")
+    content = result.stdout.splitlines()
+    print("Checking...")
+    assert result.exit_code == 0
+    assert float(content[0]) == sqrt(context["root"])
+    assert float(content[1]) == context["x"] ** context["y"]
